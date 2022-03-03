1a6da8fdf16bf627bdc7195e09981bf9a6c3ad08
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-03-02 17:52:38 -0800

    Added functionality to save, load, clear, undo and redo changes in the editor

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const initialText = `### Welcome to my React Markdown Previewer!
 
 ---
 
-#### <ins>**Headings** can be created by starting a line with '#' symbols:</ins>
+<ins>**Headings** can be created by starting a line with '#' symbols:</ins>
 
 # Heading 1 (#)
 ## Heading 2 (##)
@@ -80,12 +80,57 @@ ${'\\`\\`\\`'}
 For more Markdown Syntax, see this [Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/)
 `;
 
+// DLL is used to hold editor history
+class DLLNode {
+  constructor(editorText) {
+    this.editorText = editorText;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
 const App = () => {
   const [editorText, setEditorText] = React.useState(initialText);
+  const [savedText, setSavedText] = React.useState(initialText);
   const [previewHTML, setPreviewHTML] = React.useState(initialText);
+  const [editorHistory, setEditorHistory] = React.useState(
+    new DLLNode(initialText)
+  );
 
+  // Update EditorText and History on change
   const handleEditorChange = (event) => {
     setEditorText(event.target.value);
+    editorHistory.next = new DLLNode(event.target.value);
+    editorHistory.next.prev = editorHistory;
+    setEditorHistory(editorHistory.next);
+  };
+
+  const handleEditorUndo = () => {
+    if (editorHistory.prev) {
+      const prevHistory = editorHistory.prev;
+      setEditorText(prevHistory.editorText);
+      setEditorHistory(prevHistory);
+    }
+  };
+
+  const handleEditorRedo = () => {
+    if (editorHistory.next) {
+      const nextHistory = editorHistory.next;
+      setEditorText(nextHistory.editorText);
+      setEditorHistory(nextHistory);
+    }
+  };
+
+  const handleEditorSave = () => {
+    setSavedText(editorText);
+  };
+
+  const handleEditorLoad = () => {
+    setEditorText(savedText);
+  };
+
+  const handleEditorClear = () => {
+    setEditorText('');
   };
 
   // Update the preview view whenever the editorText changes
@@ -103,11 +148,78 @@ const App = () => {
   return (
     <>
       {/* NAVBAR / CONTROLS */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             Markdown Previewer
           </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse d-flex justify-content-between"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-info m-2"
+                  onClick={handleEditorUndo}
+                  disabled={editorHistory.prev ? false : true}
+                >
+                  Undo
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-info m-2"
+                  onClick={handleEditorRedo}
+                  disabled={editorHistory.next ? false : true}
+                >
+                  Redo
+                </button>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-primary m-2"
+                  onClick={handleEditorSave}
+                >
+                  Save Editor
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-warning m-2"
+                  onClick={handleEditorLoad}
+                >
+                  Load Editor
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-danger m-2"
+                  onClick={handleEditorClear}
+                >
+                  Clear Editor
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
 
