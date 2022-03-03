17d1d14944232664da869ccb9d27f8bce41c191e
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-03-02 20:58:51 -0800

    Moved initial editor text into a separate file

--- App.js
+++ App.js
@@ -82,7 +82,7 @@ const App = () => {
         editorHistory={editorHistory}
       />
       <div className="container">
-        <div className="d-flex flex-nowrap">
+        <div className="row">
           <MarkDownEditor
             editorText={editorText}
             handleEditorChange={handleEditorChange}
@@ -93,88 +93,5 @@ const App = () => {
     </>
   );
 };
-
-// Initial Text for Editor:
-const initialText = `### Welcome to my React Markdown Previewer!
-
----
-
-<ins>**Headings** can be created by starting a line with '#' symbols:</ins>
-
-# Heading 1 (#)
-## Heading 2 (##)
-### Heading 3 (###)
-#### Heading 4 (####)
-##### Heading 5 (#####)
-###### Heading 6 (######)
-
----
-
-<ins>**Emphasis** can be added as follows:</ins>
-
-*Italic Text* by surrounding the text with single asterisks like **\\*this\\***
-
-**Bold Text** by surrounding the text with double asterisks like **\\*\\*this\\*\\***
-
-***Bold and Italic Text*** by surrounding the text with triple asterisks like **\\*\\*\\*this\\*\\*\\***
-
-~Strikethrough Text~ by surrounding the text with single tildes  like **\\~this\\~**
-
-> blockquote text can be added by starting the line like **> this**
-
----
-
-<ins>**Code** text can be styled as follows:</ins>
-
-${'`'}Inline Code${'`'} can be created by surrounding the text with single backticks like ${'\\`'}this${'\\`'}
-
-${'```'}
-
-Multiline
-Code
-Blocks
-
-${'```'}
-
-can be created by surrounding the text with triple backticks like
-
-${'\\`\\`\\`'}
-
-this
-
-${'\\`\\`\\`'}
-
----
-
-<ins>**Lists** can be created as follows:</ins>
-
-1. Ordered
-2. Lists
-3. can be created by numbering elements like **1. this**
-
-- Unordered
-- Lists
-- can be created by starting each list item with a dash like **- this**
-
----
-
-<ins>**Horizontal Rules** can be created using three dashes (---)</ins>
-
----
-
-<ins>**Images** can be added using this syntax:</ins>
-
-**\!\\[alt text\\]\\(link_to_image.jpg\\)**
-
-![Free Code Camp Logo Green](https://plcoster.github.io/homepage/assets/Projects/FCC.png)
-
----
-
-<ins>**Links** can be added using this syntax:</ins>
-
-**\\[title\\]\\(link_to_resource\\)**
-
-For more Markdown Syntax, see this [Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/)
-`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
