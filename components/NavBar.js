f0dde85ff8ebf2147516f4c67d6ac0cfd4a2ca3b
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-03-22 18:11:07 -0700

    Added links to homepage and other projects to navbar component, added fontawesome icons

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,75 +1,106 @@
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-sm navbar-light">
+    <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href=".">
           Markdown Previewer
         </a>
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div
-          className="collapse navbar-collapse justify-content-between"
-          id="navbarNav"
-        >
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
-            {/* <li className="nav-item">
-              <button
-                type="button"
-                className="btn btn-sm btn-info m-2"
-                onClick={handleEditorUndo}
-                disabled={editorHistory.prev ? false : true}
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                href="https://plcoster.github.io/homepage/"
               >
-                Undo
-              </button>
+                Home
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                FreeCodeCamp Projects
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  <a
+                    className="dropdown-item"
+                    href="https://plcoster.github.io/fcc_frontendlibs_project1/"
+                  >
+                    Quote Generator
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="dropdown-item"
+                    href="https://plcoster.github.io/fcc_frontendlibs_project2/"
+                  >
+                    Markdown Previewer
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="dropdown-item"
+                    href="https://plcoster.github.io/fcc_frontendlibs_project3/"
+                  >
+                    Drum Machine
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="dropdown-item"
+                    href="https://plcoster.github.io/fcc_frontendlibs_project4/"
+                  >
+                    JavaScript Calculator
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="dropdown-item"
+                    href="https://plcoster.github.io/fcc_frontendlibs_project5/"
+                  >
+                    25 + 5 Clock
+                  </a>
+                </li>
+              </ul>
             </li>
             <li className="nav-item">
-              <button
-                type="button"
-                className="btn btn-sm btn-info m-2"
-                onClick={handleEditorRedo}
-                disabled={editorHistory.next ? false : true}
+              <a
+                className="nav-link"
+                href="https://plcoster.github.io/homepage/projects.html"
               >
-                Redo
-              </button>
-            </li> */}
-          </ul>
-          <ul className="navbar-nav">
-            {/* <li className="nav-item">
-              <button
-                type="button"
-                className="btn btn-sm btn-primary m-2"
-                onClick={handleEditorSave}
-              >
-                Save
-              </button>
+                All Projects
+              </a>
             </li>
             <li className="nav-item">
-              <button
-                type="button"
-                className="btn btn-sm btn-warning m-2"
-                onClick={handleEditorLoad}
-              >
-                Load
-              </button>
+              <a className="nav-link" href="https://github.com/PLCoster">
+                <i className="fa-brands fa-github"></i> Github
+              </a>
             </li>
             <li className="nav-item">
-              <button
-                type="button"
-                className="btn btn-sm btn-danger m-2"
-                onClick={handleEditorClear}
-              >
-                Clear
-              </button>
-            </li> */}
+              <a className="nav-link" href="https://linkedin.com/in/plcoster">
+                <i className="fa-brands fa-linkedin"></i> LinkedIn
+              </a>
+            </li>
           </ul>
         </div>
       </div>
